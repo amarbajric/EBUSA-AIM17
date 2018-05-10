150213fbc41a0d2f848a824c02fe4cc655d3f4ba
Felix <felix.graf@edu.fh-joanneum.at>
2018-05-10 15:47:04 +0200

    intial view approval

--- GUI-dev/src/app/pages/approval/approval.component.ts
+++ GUI-dev/src/app/pages/approval/approval.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+import {Process} from "../../../models/models";
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './approval.component.html',
+  styleUrls:['./approval.component.scss']
 })
 export class ApprovalComponent {
+
+  @Input() process:Process;
+
+  iFrameSource: string = "http://localhost:4000/#/";
+  reviews:string[] = ["Awesome process model", "This could use some serious improvement", "Subject X is missing"];
+
+  constructor(private sanitizer: DomSanitizer){
+
+  }
+
+  ngOnInit(){
+    this.process = new Process();
+    this.process.created_at = new Date();
+    this.process.process_name = "Business-Trip-Application-Process";
+    this.process.process_description = "This S-BPM process model describes how a employee applies for a business trip";
+  }
+
+  switchIFrameSource(){
+    this.iFrameSource = "http://localhost:4200/#/home";
+  }
+
+  transformUrl(url:string):any {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+
 }
--- GUI-dev/src/models/models.ts
+++ GUI-dev/src/models/models.ts
@@ -3,7 +3,36 @@
 
 export class Process {
   // define props...
+  process_id?: number;
+  created_at?: Date;
+  price?: number;
+  process_description?: string;
+  process_name?: string;
+  state?: string;
+  version?: number;
+  u_id?: number;
 }
+
+export class Rating {
+  // define props...
+  rating_id?: number;
+  comment?: string;
+  rating?: number;
+  process_id: number;
+  u_id?: number;
+}
+
+export class Review {
+  // define props...
+  review_id?: number;
+  comment?: string;
+  is_approved?: number;
+  u_id?: number;
+  process_id: number;
+  approver: number;
+  uploader: number;
+}
+
 
 
 export class User {
