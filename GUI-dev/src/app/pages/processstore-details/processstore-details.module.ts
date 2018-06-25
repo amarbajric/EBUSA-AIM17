128f6de22baf665fec387ad9e9fc21167a51a60d
spalekni14 <nina.spalek@edu.fh-joanneum.at>
2018-06-25 19:48:08 +0200

    deleted non available import

--- GUI-dev/src/app/pages/processstore-details/processstore-details.module.ts
+++ GUI-dev/src/app/pages/processstore-details/processstore-details.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { ProcessRatingModule } from '../includes/process-ratings/process-ratings.module';
 import { ProcessStoreDetailsComponent } from './processstore-details.component';
-import {ModalProcessstoreDetailsComponent} from "./modal/modal-processstore-details.component";
 import {ProcessstoreDetailsModal} from "./modal/processstore-details.modal";
 
 @NgModule({
