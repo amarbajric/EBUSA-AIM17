05731f3676d7dd2137cfa8533c819f65cf1c27cc
Felix <felix.graf@edu.fh-joanneum.at>
2018-06-25 22:15:48 +0200

    linting

--- GUI-dev/src/app/pages/approval-details/approval-details.component.ts
+++ GUI-dev/src/app/pages/approval-details/approval-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Inject} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { StoreProcess} from '../../../models/models';
 import {Review} from '../../../models/models';
 import { GatewayProvider } from '../../@theme/providers/backend-server/gateway';
