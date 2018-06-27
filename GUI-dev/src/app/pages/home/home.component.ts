9e2b958b550fbb0fe6071f8224e44bd9a4ceb5f0
Michael Fuchs <michael.fuchs@edu.fh-joanneumm.at>
2018-06-27 03:43:04 +0200

    union type bugfix

--- GUI-dev/src/app/pages/home/home.component.ts
+++ GUI-dev/src/app/pages/home/home.component.ts
@@ -81,8 +81,7 @@ export class HomeComponent implements OnInit {
           }
         });
         for (const element of combination) {
-          // shows as an error but works ...
-          this.processesByRating.push(element[1]);
+          this.processesByRating.push(element[1] as StoreProcess);
         }
 
         this.processesByRating = this.processesByRating.slice(0, this.limit);
